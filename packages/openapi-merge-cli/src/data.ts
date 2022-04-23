c84bc427830685c963a0c98424d0adaf5142f9d8
Revanth <revanth.g@iouring.com>
2022-04-23 16:31:36 +0530

    title and info added in config schema

--- packages/openapi-merge-cli/src/data.ts
+++ packages/openapi-merge-cli/src/data.ts
@@ -215,6 +215,8 @@ export type Configuration = {
    */
   inputs: ConfigurationInput[];
   servers: Swagger.Server[];
+  title: string;
+  description: string;
   /**
    * The output file to put the results in. If you use the .yml or .yaml extension then the schema will be output
    * in YAML format, otherwise, it will be output in JSON format.
